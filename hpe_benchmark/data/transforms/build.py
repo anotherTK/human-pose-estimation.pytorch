8f33029e6f5380c0b9e847410203911705e2d43e
tkianai <tkianai@163.com>
2019-06-27 09:51:18 +0800

    add data augment

--- hpe_benchmark/data/transforms/build.py
+++ hpe_benchmark/data/transforms/build.py
@@ -1,3 +1,4 @@
+import random
 import torchvision.transforms as transforms
 
 def build_transforms(cfg, is_train=True):
@@ -23,11 +24,38 @@ def build_transforms(cfg, is_train=True):
         hue=hue,
     )
 
-    transform = transforms.Compose(
+    transform = Compose(
         [
             color_jitter,
+        ],
+        [
             transforms.ToTensor(),
             normalize,
-        ]
+        ],
     )
     return transform
+
+
+class Compose(object):
+    def __init__(self, random_transforms, transforms):
+        self.random_transforms = random_transforms
+        self.transforms = transforms
+
+    def __call__(self, image):
+        for t in self.random_transforms:
+            if random.random() > 0.5:
+                image = t(image)
+        for t in self.transforms:
+            image = t(image)
+        return image
+
+    def __repr__(self):
+        format_string = self.__class__.__name__ + "("
+        for t in self.transforms:
+            format_string += "\n"
+            format_string += "    {0}".format(t)
+        for t in self.random_transforms:
+            format_string += "\n"
+            format_string += "    {0}".format(t)
+        format_string += "\n)"
+        return format_string
